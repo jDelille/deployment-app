eead3d5754edfaf0209dcae9a3c93adb26ff8abe
Justin Delille <justind7513@gmail.com>
2021-09-23 11:12:50 -0700

    stylesheet added

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const path = require('path')
 const app = express();
 const port = process.env.PORT || 4400
 
+app.use(express.static("client"));
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 app.listen(port, () => {
